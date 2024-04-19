66831fb081c2e1f02453ce15b68560dcce9a9542
sam-starikov <sam_starikov@rambler.ru>
2024-04-19 09:24:06 +0400

    first comit

---
+++ script.js
@@ -0,0 +1,9 @@
+/* burger */
+const burger = document.querySelector('.burger')
+const menu = document.querySelector('.menu')
+
+burger.addEventListener('click', () => {
+	burger.classList.toggle('burger--active')
+	menu.classList.toggle('menu--active')
+})
+
